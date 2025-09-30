f1cdf4f842a4a81720a97f7056caf619d5102cf6
nhan.nt <cd15km1@gmail.com>
2025-09-30 23:16:33 +0700

    test bluetooth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,30 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./App.tsx",
+//     //"./creens/*.{js,jsx,ts,tsx}",
+//     //"./**/*.{js,jsx,ts,tsx}"
+//   ],
+//   presets: [require("nativewind/preset")],
+//   theme: {
+//     extend: {
+//       colors: {
+//         // Use your preferred name and color
+//         main: "#023047",
+//       }
+//     },
+//   },
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
   content: [
-    "./App.tsx",
-    "./creens/*.{js,jsx,ts,tsx}",
-    "./**/*.{js,jsx,ts,tsx}"
+    "./App.{js,jsx,ts,tsx}",
+    "./creens/**/*.{js,jsx,ts,tsx}"
   ],
-  presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      colors: {
-        // Use your preferred name and color
-        main: "#023047",
-      }
-    },
+    extend: {},
   },
+  plugins: [],
 }
