09b08f9ed67660d9f218aa131edc8822c823f87d
nhan.nt <cd15km1@gmail.com>
2025-10-04 11:06:38 +0700

    test main scanner

--- components/Main/MainDeviceList.tsx
+++ components/Main/MainDeviceList.tsx
@@ -1,41 +1,124 @@
-import { Text, View, Pressable, Modal, FlatList } from 'react-native';
+import { Text, View, Pressable, Modal, FlatList, Alert } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { BleManager, Device, State } from 'react-native-ble-plx';
+import MainDeviceStatus from './MainDeviceStatus';
+//import { KrossDevice } from '../../ble/KrossDevice';
+// import useScanDevice from '../../hooks/ble/useScanDevice';
 
-//temp code
-import MainCardDevices from './MainCardDevices';
-import { Device } from 'react-native-ble-plx';
-import useScanDevice from '../../hooks/ble/useScanDevice';
+// type ScannedDevice = {
+// 	id: string;
+// 	name: string | null;
+// 	rssi: number | null;
+// 	device: Device;
+// };
 
-type ScannedDevice = {
-	id: string;
-	name: string | null;
-	rssi: number | null;
-	device: Device;
-};
+const SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+const DATA_IN_UUID = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+const DATA_OUT_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
 
 const MainDeviceList = () => {
 
-	const [selected, setSelected] = useState<ScannedDevice>();
+	const managerRef = useRef<BleManager | null>(null);
+	const [isScanning, setIsScanning] = useState(false);
+	const [devices, setDevices] = useState<Device[]>([]);
+	const [device, setSelectedDevice] = useState<Device>();
 	const [open, setOpen] = useState(false);
+	//const [, setStateVersion] = useState(0);
+	//const krossDevice = new KrossDevice();
 
-	const [device, setDevice] = useState<ScannedDevice[]>([]);
-	const devices = useScanDevice();
-	const dv = Array.from(devices.values())
+	console.log(`MainDeviceList render!`)
 
 	useEffect(() => {
-		setDevice(dv);
-		setSelected(dv[0])
-	}, [])
+		let manager: BleManager;
+		let sub: any;
+
+		try {
+			manager = new BleManager();
+			managerRef.current = manager;
+
+			sub = manager.onStateChange((state: State) => {
+				if (state === 'PoweredOff') {
+					Alert.alert('Bluetooth off', 'Turn off device to Scan!');
+				}
+			}, true);
+		} catch (error) {
+			Alert.alert('Can not init BleManager');
+		}
+
+		startScan();
+
+		return () => {
+			try {
+				sub.remove();
+				stopScan();
+				manager.destroy();
+				managerRef.current = null;
+			} catch (cleanupError) {
+				//console.error("Error to cleanup BleManager:", cleanupError);
+			}
+		};
+	}, []);
+
+	const startScan = async () => {
+		const manager = managerRef.current;
+		if (!manager) return;
+
+		// Clear previous results
+		setDevices([]);
+
+		try {
+			// On iOS you don't need to request runtime permission here (Info.plist required)
+			// On Android you must request ACCESS_FINE_LOCATION / BLUETOOTH_SCAN depending on SDK level.
+			setIsScanning(true);
+			manager.startDeviceScan([SERVICE_UUID], { allowDuplicates: false }, (error, scannedDevice) => {
+				if (error) {
+					Alert.alert('Scan error', `${error.message}`);
+					setIsScanning(false);
+					return;
+				}
+
+				if (scannedDevice) {
+					setDevices(prev => [...prev, scannedDevice]);
+				}
+
+				if (!device) {
+					setSelectedDevice(devices[0])
+				}
+
+			})
+
+			// Auto-stop scan after 10 seconds to save battery
+			setTimeout(() => {
+				stopScan();
+			}, 5000);
+
+		} catch (e: any) {
+			//console.warn('Failed to start scan', e);
+			Alert.alert('Failed to start scan', e?.message ?? String(e));
+			setIsScanning(false);
+		}
+	};
+
+	const stopScan = () => {
+		const manager = managerRef.current;
+		try {
+			manager?.stopDeviceScan();
+		} catch (e) {
+			console.warn('stopScan error', e);
+		}
+		setIsScanning(false);
+	};
 
 	return (
 		<View className="px-4">
 			{/* Card for device status (contains combobox) */}
-			<MainCardDevices
-				devices={selected}
+			<MainDeviceStatus
+				device={device}
 				setOpen={() => setOpen(true)}
+				managerRef={managerRef}
 			>
-			</MainCardDevices>
+			</MainDeviceStatus>
 
 			{/* Modal for selecting device */}
 			<Modal
@@ -53,12 +136,12 @@ const MainDeviceList = () => {
 						<Text className="text-gray-700 font-semibold mb-3">Select device</Text>
 
 						<FlatList
-							data={device}
+							data={devices}
 							keyExtractor={(item) => item.id}
 							renderItem={({ item }) => (
 								<Pressable
 									onPress={() => {
-										setSelected(item);
+										setSelectedDevice(item);
 										setOpen(false);
 									}}
 									className="px-3 py-3 rounded-xl mb-2"
@@ -67,7 +150,7 @@ const MainDeviceList = () => {
 										{/* <View className={`w-2 h-2 rounded-full bg-${item.color}-500 mr-2`} /> */}
 										<Text className="text-gray-700">{item.name}</Text>
 										<View className="w-4" />
-										{item.id === selected?.id ? (
+										{item.id === device?.id ? (
 											<Ionicons className="ml-auto text-base" name="checkmark" size={15} color="gray" />
 										) : null}
 									</View>
@@ -87,4 +170,4 @@ const MainDeviceList = () => {
 	)
 }
 
-export default MainDeviceList
+export default MainDeviceList;
---
+++ components/Main/MainDeviceStatus.tsx
@@ -0,0 +1,113 @@
+import { styled } from "nativewind";
+import { StyleSheet, Text, View, Pressable, Alert } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { colorMap, colorMapBg, type ColorVariant } from '../../model/DotStatusColor';
+import { BleManager, Device } from "react-native-ble-plx";
+import { KrossDevice } from '../../ble/KrossDevice';
+import { useEffect, useState } from "react";
+
+//const Icon = styled(Ionicons);
+
+type StatusDevice = {
+	id: string;
+	name: string | null;
+	status: string | null;
+	battery: number | null;
+	color: string;
+};
+
+const MainDeviceStatus = ({ device, setOpen, managerRef, }: { device: Device | undefined, setOpen: any, managerRef: React.RefObject<BleManager | null> }) => {
+
+	const SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
+	const DATA_OUT_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
+	const krossDevice = new KrossDevice();
+	//const [isConnected, setIsConnected] = useState(false);
+	const [dv, setDevice] = useState<StatusDevice>();
+
+	console.log(`MainDeviceStatus render!`)
+
+	useEffect(() => {
+		connectToDevice();
+	}, []);
+
+	const connectToDevice = async () => {
+		try {
+			const connected = await managerRef.current?.connectToDevice(device ? device.id : "", { autoConnect: true });
+			if (!connected) {
+				Alert.alert('Connect error', 'No connected device');
+				return;
+			}
+
+			await connected.discoverAllServicesAndCharacteristics();
+			const subscription = await connected.monitorCharacteristicForService(
+				SERVICE_UUID,
+				DATA_OUT_UUID,
+				(error, char) => {
+					if (error) {
+						//console.error("Notification error:", error);
+						return;
+					}
+
+					let data = krossDevice.onDataReceived(KrossDevice.decodeBase64(char?.value ?? ""));
+					if (data) {
+						krossDevice.unpack(data);
+						setDevice({
+							id: "",
+							battery: krossDevice.soc,
+							name: krossDevice.name,
+							status: "Available",
+							color: (1 == 1) ? "red" : "blue"
+						});
+						subscription.remove();
+					} else {
+						// console.log("Received data is null");
+					}
+				}
+			);
+
+		} catch (e: any) {
+			Alert.alert('connect error', e?.message ?? String(e));
+		}
+	};
+
+	const color: ColorVariant = "green";
+	const colorbg: ColorVariant = "green";
+
+	return (
+		<View className="bg-white rounded-2xl p-4 shadow">
+			<View className="flex-row items-center space-x-2 mb-2">
+				<Text className="text-gray-700 font-semibold">Device Status</Text>
+				<View className="w-20"></View>
+				<View className="flex-row justify-end items-center space-x-2">
+					{/* Battery icon */}
+					<Ionicons name="battery-half-outline" size={18} />
+					<Text className="font-medium">{dv?.battery}</Text>
+					{/* <Text className={`${colorMap[dv?.color]} font-medium`}>{dv?.battery}</Text> */}
+
+					{/* Status dot */}
+					{/* <View className={`w-2.5 h-2.5 rounded-full ${colorMapBg[colorbg]} ml-2`} /> */}
+					{/* <Text className={`text-${devices.color}-600 font-medium`}>{devices.status}</Text> */}
+					<Text className="text--600 font-medium">{dv?.status}</Text>
+				</View>
+			</View>
+
+			{/* Combobox row */}
+			<Pressable
+				onPress={() => setOpen(true)}
+				className="flex-row items-center justify-between bg-gray-50 rounded-xl px-4 py-3"
+			>
+				<View className="flex-row items-center">
+					{/* green dot */}
+					{/* <View className={`w-3 h-3 rounded-full ${colorMapBg[colorbg]} mr-3`} /> */}
+					<Text className="text-gray-700">{dv?.name}</Text>
+				</View>
+
+				<Text className="text-gray-400">
+					<Ionicons name="chevron-down" size={15} color="gray" />
+				</Text>
+			</Pressable>
+		</View>
+	)
+}
+
+export default MainDeviceStatus
