4ada3db170d200eedc06abc910e0ad6afc5d6896
HAI NAM CHU <namc@minitcharger.com>
2025-10-04 17:12:25 +0700

    upgrade main scanner

--- components/Main/MainDeviceStatus.tsx
+++ components/Main/MainDeviceStatus.tsx
@@ -1,12 +1,12 @@
 import { styled } from "nativewind";
-import { StyleSheet, Text, View, Pressable, Alert } from 'react-native'
+import { Text, View, Pressable, Alert } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { colorMap, colorMapBg, type ColorVariant } from '../../model/DotStatusColor';
+// import { colorMap, colorMapBg, type ColorVariant } from '../../model/DotStatusColor';
 import { BleManager, Device } from "react-native-ble-plx";
 import { KrossDevice } from '../../ble/KrossDevice';
 import { useEffect, useState } from "react";
 
-//const Icon = styled(Ionicons);
+const Icon = styled(Ionicons);
 
 type StatusDevice = {
 	id: string;
@@ -16,36 +16,44 @@ type StatusDevice = {
 	color: string;
 };
 
-const MainDeviceStatus = ({ device, setOpen, managerRef, }: { device: Device | undefined, setOpen: any, managerRef: React.RefObject<BleManager | null> }) => {
-
+const MainDeviceStatus = ({ deviceId, setOpen, managerRef, }: { deviceId: string, setOpen: any, managerRef: React.RefObject<BleManager | null> }) => {
 	const SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
 	const DATA_OUT_UUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
 	const krossDevice = new KrossDevice();
-	//const [isConnected, setIsConnected] = useState(false);
-	const [dv, setDevice] = useState<StatusDevice>();
+	const [dvInfo, setDvInfo] = useState<StatusDevice>();
+	const [localDeviceId, setLocalDeviceId] = useState<string>(deviceId);
 
-	console.log(`MainDeviceStatus render!`)
-
+	//console.log(`MainDeviceStatus render with deviceId! ${deviceId}`)
+	
 	useEffect(() => {
-		connectToDevice();
+		//console.log(`MainDeviceStatus useEffect render! ${deviceId}`)
+		setLocalDeviceId(deviceId)
+		connectToDevice(deviceId)
 
-		return () => {
-			//managerRef.current = null;
-		}
+	}, [deviceId]);
 
-	}, []);
-
-	const connectToDevice = async () => {
+	const connectToDevice = async (deviceId: string) => {
+		//console.log(`MainDeviceStatus -- Run connectToDevice with deviceId: ${deviceId}`);
 		try {
-			const connected = await managerRef.current?.connectToDevice(device ? device.id : "", { autoConnect: true });
-			if (!connected) {
-				//Alert.alert('Connect error', `No connected device: `);
-				console.log(`MainDeviceStatus -- Connect error No connected device: ${device ? device.id : ""}`);
+			
+			if (deviceId === "") {
 				return;
 			}
 
+			stopScan();
+			const connected = await managerRef.current?.connectToDevice(deviceId, { autoConnect: true });
+			if (!connected) {
+				//Alert.alert('Connect error', `No connected device: `);
+				//console.log(`MainDeviceStatus -- Connect error No connected device: ${deviceId}`);
+				return;
+			}
+
+			const tranSactionID = "GET_STAGE_DEVICE";
+
+			//console.log(`MainDeviceStatus -- Connect success: ${deviceId}`);
+
 			await connected.discoverAllServicesAndCharacteristics();
-			const subscription = await connected.monitorCharacteristicForService(
+			let subscription = connected.monitorCharacteristicForService(
 				SERVICE_UUID,
 				DATA_OUT_UUID,
 				(error, char) => {
@@ -54,32 +62,56 @@ const MainDeviceStatus = ({ device, setOpen, managerRef, }: { device: Device | u
 						return;
 					}
 
-					console.log(`MainDeviceStatus - connectToDevice - monitorCharacteristicForService`)
-
+					//onsole.log(`MainDeviceStatus - connectToDevice - monitorCharacteristicForService`)
 					let data = krossDevice.onDataReceived(KrossDevice.decodeBase64(char?.value ?? ""));
 					if (data) {
 						krossDevice.unpack(data);
-						setDevice({
-							id: "",
-							battery: krossDevice.soc,
-							name: krossDevice.name,
-							status: "Available",
-							color: (1 == 1) ? "red" : "blue"
-						});
-						subscription.remove();
-					} else {
-						// console.log("Received data is null");
-					}
-				}
-			);
 
+						setDvInfo((prev) => {
+							if (prev?.battery === krossDevice.soc) return prev;
+								return {
+									id: "",
+									battery: krossDevice.soc,
+									name: connected.name, 
+									status: "Connected",
+									color: (krossDevice.soc >= 60)
+											? 
+												"green" 
+											: 
+												(krossDevice.soc > 25 && krossDevice.soc < 60) 
+													? 
+														"yellow" 
+													: 
+														"red",
+								};
+						});
+
+						setTimeout(() => {
+							managerRef.current?.cancelTransaction(tranSactionID);
+						}, 1000);
+
+					} else {
+					// 	// console.log("Received data is null");
+					}
+				},
+				tranSactionID
+			);
 		} catch (e: any) {
-			Alert.alert('connect error', e?.message ?? String(e));
+			//Alert.alert('connect error', e?.message ?? String(e));
 		}
 	};
 
-	const color: ColorVariant = "green";
-	const colorbg: ColorVariant = "green";
+	const stopScan = () => {
+		const manager = managerRef.current;
+		try {
+			manager?.stopDeviceScan();
+		} catch (e) {
+			//console.warn('stopScan error', e);
+		}
+	};
+
+	// const color: ColorVariant = "green";
+	// const colorbg: ColorVariant = "green";
 
 	return (
 		<View className="bg-white rounded-2xl p-4 shadow">
@@ -88,14 +120,18 @@ const MainDeviceStatus = ({ device, setOpen, managerRef, }: { device: Device | u
 				<View className="w-20"></View>
 				<View className="flex-row justify-end items-center space-x-2">
 					{/* Battery icon */}
-					<Ionicons name="battery-half-outline" size={18} />
-					<Text className="font-medium">{dv?.battery}</Text>
-					{/* <Text className={`${colorMap[dv?.color]} font-medium`}>{dv?.battery}</Text> */}
-
-					{/* Status dot */}
-					{/* <View className={`w-2.5 h-2.5 rounded-full ${colorMapBg[colorbg]} ml-2`} /> */}
-					{/* <Text className={`text-${devices.color}-600 font-medium`}>{devices.status}</Text> */}
-					<Text className="text--600 font-medium">{dv?.status}</Text>
+					{
+						dvInfo && dvInfo?.battery
+						?
+						<View className="flex-row items-center space-x-2">
+							<Icon name="battery-half-outline" className={`text-${dvInfo.color}-600`} size={18} />
+							<Text className={`text-${dvInfo.color}-600`}>{dvInfo?.battery}%</Text>
+							<View className="w-2.5 h-2.5 rounded-xl bg-green-500 ml-2" />
+							<Text className="text-green-600 font-medium">{dvInfo?.status}</Text>
+						</View>
+						:
+						<></>
+					}
 				</View>
 			</View>
 
@@ -106,8 +142,16 @@ const MainDeviceStatus = ({ device, setOpen, managerRef, }: { device: Device | u
 			>
 				<View className="flex-row items-center">
 					{/* green dot */}
-					{/* <View className={`w-3 h-3 rounded-full ${colorMapBg[colorbg]} mr-3`} /> */}
-					<Text className="text-gray-700">{dv?.name}</Text>
+					{
+						dvInfo && dvInfo?.battery
+						?
+						<>
+							<View className="w-3 h-3 rounded-xl bg-green-500 mr-3" />
+							<Text className="text-gray-700">{dvInfo?.name}</Text>
+						</>
+						:
+						<></>
+					}
 				</View>
 
 				<Text className="text-gray-400">
