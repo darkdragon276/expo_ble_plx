60d0d81d4d04b0ce5a8e614ff8deb14e05ae63ac
HAI NAM CHU <namc@minitcharger.com>
2025-10-11 13:23:37 +0700

    update assessment

--- components/RangeOfMotion/AssessmentLLateral.tsx
+++ components/RangeOfMotion/AssessmentLLateral.tsx
@@ -1,7 +1,8 @@
-import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
+import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from "react";
 import { View, Text } from "react-native";
 import { ChildROMRef } from "../../model/ChildRefGetValue";
 import { bleEventEmitter } from "../../utils/BleEmitter";
+import { normalizeAngle } from "../../utils/helper";
 
 let l_lateral: number = 0.0;
 type AssessmentCardProps = {
@@ -9,17 +10,19 @@ type AssessmentCardProps = {
 };
 
 const AssessmentLLateral = forwardRef<ChildROMRef, AssessmentCardProps>(({ record }, ref) => {
-	const [pos, setPos] = useState<number>(0.0)
-	const [posMax, setPosMax] = useState<number>(0.0)
+	const [pos, setPos] = useState<number>(0.0);
+	const [posMax, setPosMax] = useState<number>(0.0);
+	const Offset = useRef<number | null>(null);
 
 	useEffect(() => {
 		const sub = bleEventEmitter.addListener('BleDataRoll', (data: number) => {
-			l_lateral = Math.round(data * 10) / 10;
-			if (l_lateral < 0) {
-				setPos(l_lateral * -1);
-			} else {
-				setPos(0.0);
+			if (Offset.current === null) {
+				// assign ONCE the first time listener is called
+				Offset.current = Math.round(data);
 			}
+
+			let alpha = normalizeAngle(Math.round(data * 10) / 10 - Offset.current);
+			setPos(Math.round((alpha <= 0 ? -alpha : 0) * 10) / 10);
 			setPosMax((pos > posMax) ? pos : posMax);
 		});
 
