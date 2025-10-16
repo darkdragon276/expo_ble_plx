31bf6d40600f9c766bd2be0ce68efba4632e2912
HAI NAM CHU <namc@minitcharger.com>
2025-10-16 22:16:42 +0700

    Update icon Roll, Pitch

--- creens/RangeOfMotionSummary.tsx
+++ creens/RangeOfMotionSummary.tsx
@@ -93,13 +93,13 @@ const TitleSummary = ({ title, dataKey }: { title: string, dataKey: string }) =>
 				!edit
 					?
 					<>
-						<Text className="w-rounded-xl px-2 py-2 text-lg">{text}</Text>
+						<Text className="w-rounded-xl font-bold px-2 py-2 text-lg">{text}</Text>
 						<LuPenLine size={20} color="gray" onPress={() => setEditText(true)}></LuPenLine>
 					</>
 					:
 					<>
 						<TextInput
-							className="w-rounded-xl px-2 py-2 text-lg"
+							className="w-rounded-xl font-bold px-2 py-2 text-lg"
 							defaultValue={text}
 							onChangeText={newText => setText(newText)}
 							placeholderTextColor="black"
