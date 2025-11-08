40f5561a77bbdf74c13ecd812ebf96da2886130d
nhan.nt <cd15km1@gmail.com>
2025-11-08 15:51:14 +0700

    fix bug ROM PDF

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,7 +1,20 @@
+import { Asset } from "expo-asset";
+import * as FileSystem from 'expo-file-system/legacy';
+
 const normalizeAngle = (alpha: number): number => {
     alpha = ((alpha + 180) % 360 + 360) % 360;
     return alpha - 180;
 };
 
+// convert imge to base64
+const loadImg = async (localSrc: any) => {
+    const src = Asset.fromModule(localSrc)
+    await src.downloadAsync();
+    const base64 = await FileSystem.readAsStringAsync(src.localUri || "", {
+        encoding: "base64"
+    });
 
-export { normalizeAngle }
+    return `data:image/png;base64,${base64}`;
+}
+
+export { normalizeAngle, loadImg }
