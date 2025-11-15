ee9dd5a0834e0c4b75ea4fc866accfd668a58f9e
nhan.nt <cd15km1@gmail.com>
2025-11-15 16:49:06 +0700

    update Live Cursor

--- components/JointPositionSense/LiveCursor.tsx
+++ components/JointPositionSense/LiveCursor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { View, StyleSheet, Animated, Alert } from 'react-native';
 import { type LiveHeadPositionProps } from '../../model/JointPosition';
 import { BLEService } from '../../ble/BLEService';
@@ -7,6 +7,8 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
 import { BleError, BleErrorCode, Characteristic } from 'react-native-ble-plx';
 import { KrossDevice } from '../../ble/KrossDevice';
+import { normalizeAngle } from '../../utils/helper';
+import { X } from 'lucide-react-native';
 
 const CIRCLE_RADIUS = 85;
 const CURSOR_RADIUS = 10;
@@ -17,30 +19,32 @@ const LiveCursor = ({ dataRef, reset, record }: { dataRef: React.RefObject<LiveH
 	const navigation = useNavigation<NavigationProp>();
 	const animatedPos = useState(new Animated.ValueXY({ x: 0, y: 0 }))[0];
 	const krossDevice = new KrossDevice();
+	const OffsetX = useRef<number | null>(null);
+	const OffsetY = useRef<number | null>(null);
 
 	useEffect(() => {
 
-		const reStart = () => {
-			setTimeout(() => {
-				reset.current = false;
-			}, 1000);
-		}
+		// const reStart = () => {
+		// 	setTimeout(() => {
+		// 		reset.current = false;
+		// 	}, 1000);
+		// }
 
-		// dummy position for test
-		const interval = setInterval(() => {
-			const t = Date.now() / 1000;
-			let x = 100 * Math.cos(t);
-			let y = 120 * Math.sin(t * 1.5);
-			if (reset.current) {
-				reStart();
-				x = 0;
-				y = 0;
-			}
-			updateCursorPosition(x, y);
-		}, 50);
+		// // dummy position for test
+		// const interval = setInterval(() => {
+		// 	const t = Date.now() / 1000;
+		// 	let x = 100 * Math.cos(t);
+		// 	let y = 120 * Math.sin(t * 1.5);
+		// 	if (reset.current) {
+		// 		reStart();
+		// 		x = 0;
+		// 		y = 0;
+		// 	}
+		// 	updateCursorPosition(x, y);
+		// }, 50);
 
 		if (BLEService.deviceId === null) {
-			Alert.alert('Device disconnected', `Force stop ROM session!`, [
+			Alert.alert('Device disconnected', `Force stop JPS session!`, [
 				{
 					text: 'OK',
 					//onPress: () => navigation.replace("Main"),
@@ -49,19 +53,19 @@ const LiveCursor = ({ dataRef, reset, record }: { dataRef: React.RefObject<LiveH
 			return;
 		};
 
-		// BLEService.startSequence();
-		// liveData();
+		BLEService.startSequence();
+		liveData();
 
 		return () => {
-			clearInterval(interval);
-			// try {
-			// 	if (BLEService.getDevice() != null) {
-			// 		BLEService.cancelTransaction(BLEService.READ_DATA_TRANSACTION_ID);
-			// 	}
-			// } catch (cleanupError) {
-			// 	////console.error("Error to cleanup BleManager:", cleanupError);
-			// }
-			// BLEService.stopSequence();
+			//clearInterval(interval);
+			try {
+				if (BLEService.getDevice() != null) {
+					BLEService.cancelTransaction(BLEService.READ_DATA_TRANSACTION_ID);
+				}
+			} catch (cleanupError) {
+				////console.error("Error to cleanup BleManager:", cleanupError);
+			}
+			BLEService.stopSequence();
 		}
 	}, []);
 
@@ -71,63 +75,66 @@ const LiveCursor = ({ dataRef, reset, record }: { dataRef: React.RefObject<LiveH
 		}, 1000);
 	}
 
-	// const liveData = async () => {
-	// 	const onError = (error: BleError): void => {
-	// 		if (BLEService.isDisconnectError(error) ||
-	// 			error.errorCode === BleErrorCode.CharacteristicNotifyChangeFailed ||
-	// 			error.errorCode === BleErrorCode.CharacteristicReadFailed) {
-	// 			BLEService.deviceId = null;
-	// 			Alert.alert('Device disconnected', `Force stop ROM session!`, [
-	// 				{
-	// 					text: 'OK',
-	// 					onPress: () => { rejectCauseDisconect() }
-	// 				}
-	// 			]);
-	// 		}
-	// 	};
+	const liveData = async () => {
+		const onError = (error: BleError): void => {
+			if (BLEService.isDisconnectError(error) ||
+				error.errorCode === BleErrorCode.CharacteristicNotifyChangeFailed ||
+				error.errorCode === BleErrorCode.CharacteristicReadFailed) {
+				BLEService.deviceId = null;
+				Alert.alert('Device disconnected', `Force stop JPS session!`, [
+					{
+						text: 'OK',
+						onPress: () => { rejectCauseDisconect() }
+					}
+				]);
+			}
+		};
 
-	// 	const onMonitor = (char: Characteristic) => {
-	// 		let data = krossDevice.onDataReceived(KrossDevice.decodeBase64(char?.value ?? ""));
-	// 		let x, y;
-	// 		if (data) {
-	// 			krossDevice.unpack(data);
+		const onMonitor = (char: Characteristic) => {
+			let data = krossDevice.onDataReceived(KrossDevice.decodeBase64(char?.value ?? ""));
+			let x, y;
+			if (data) {
+				krossDevice.unpack(data);
 
-	// 			x = krossDevice.angle.pitch;
-	// 			y = krossDevice.angle.yaw
+				x = krossDevice.angle.yaw;
+				y = krossDevice.angle.pitch
 
-	// 			if (reset.current) {
-	// 				reStart();
-	// 				x = 0;
-	// 				y = 0;
-	// 			}
-	// 			updateCursorPosition(x, y);
-	// 		}
-	// 		//console.log("Monitor: ", char?.value);
-	// 	}
+				if (reset.current) {
+					OffsetX.current = 0;
+					OffsetY.current = 0;
+					reStart();
+				}
 
-	// 	await BLEService.discoverAllServicesAndCharacteristicsForDevice()
-	// 		.then(() => {
-	// 			BLEService.setupMonitor(BLEService.SERVICE_UUID, BLEService.DATA_OUT_UUID, onMonitor, onError, BLEService.READ_DATA_TRANSACTION_ID);
-	// 		})
-	// 		.catch((error) => {
-	// 			if (BLEService.isDisconnectError(error)) {
-	// 				BLEService.deviceId = null;
-	// 				Alert.alert('Device disconnected', `Force stop ROM session!`, [
-	// 					{
-	// 						text: 'OK',
-	// 						onPress: () => { rejectCauseDisconect() }
-	// 					}
-	// 				]);
-	// 			}
-	// 		});
-	// }
+				x = getHorizontalOffset(x);
+				y = getVerticalOffset(y);
+				updateCursorPosition(x, y);
+			}
+			//console.log("Monitor: ", char?.value);
+		}
 
-	// const rejectCauseDisconect = async () => {
-	// 	await BLEService.cancelTransaction(BLEService.READ_DATA_TRANSACTION_ID);
-	// 	setTimeout(async () => {
-	// 		navigation.replace("Main");
-	// 	}, 50)
-	// };
+		await BLEService.discoverAllServicesAndCharacteristicsForDevice()
+			.then(() => {
+				BLEService.setupMonitor(BLEService.SERVICE_UUID, BLEService.DATA_OUT_UUID, onMonitor, onError, BLEService.READ_DATA_TRANSACTION_ID);
+			})
+			.catch((error) => {
+				if (BLEService.isDisconnectError(error)) {
+					BLEService.deviceId = null;
+					Alert.alert('Device disconnected', `Force stop JPS session!`, [
+						{
+							text: 'OK',
+							onPress: () => { rejectCauseDisconect() }
+						}
+					]);
+				}
+			});
+	}
+
+	const rejectCauseDisconect = async () => {
+		await BLEService.cancelTransaction(BLEService.READ_DATA_TRANSACTION_ID);
+		setTimeout(async () => {
+			navigation.replace("Main");
+		}, 50)
+	};
 
 	// limits within a circle
 	const updateCursorPosition = (x: number, y: number) => {
@@ -153,8 +160,46 @@ const LiveCursor = ({ dataRef, reset, record }: { dataRef: React.RefObject<LiveH
 		dataRef.current = {
 			horizontal: x,
 			vertical: y,
-			current: "Right Extension"
+			current: getCurrentPositionText(x, y)
 		};
+	};
+
+	const getCurrentPositionText = (x: number, y: number) => {
+		let current = "";
+
+		if (x <= 0) {
+			current = "Left ";
+		} else if (x >= 0) {
+			current = "Right ";
+		}
+
+		if (y <= 0) {
+			current += "Flexion ";
+		} else if (y >= 0) {
+			current += "Extension ";
+		}
+
+		return current;
+	};
+
+	const getHorizontalOffset = (x: number): number => {
+		if (OffsetX.current === null || OffsetX.current === 0) {
+			// assign ONCE the first time listener is called
+			OffsetX.current = Math.round(x);
+		}
+
+		let alpha = normalizeAngle(Math.round(x * 10) / 10 - OffsetX.current);
+		return (Math.round(alpha * 10) / 10);
+	};
+
+	const getVerticalOffset = (y: number): number => {
+		if (OffsetY.current === null || OffsetY.current === 0) {
+			// assign ONCE the first time listener is called
+			OffsetY.current = Math.round(y);
+		}
+
+		let alpha = normalizeAngle(Math.round(y * 10) / 10 - OffsetY.current);
+		return (Math.round(alpha * 10) / 10);
 	};
 
 	return (
