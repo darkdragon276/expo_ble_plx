7048958f70df5ca0e6e4e53cfe89104c01c072e1
nhan.nt <cd15km1@gmail.com>
2025-11-19 01:25:26 +0700

    fix layout maker cursor

--- components/JointPositionSense/MarkerCursor.tsx
+++ components/JointPositionSense/MarkerCursor.tsx
@@ -6,25 +6,21 @@ const CURSOR_RADIUS = CURSOR_ADJUST_OFFSET;
 
 const MarkerCursor = ({ id, x, y, z }: { id: string, x: number, y: number, z?: number | null }) => {
 	return (
-		<View className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-			<View className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-				style={[
-					styles.cursor,
-					{
-						transform: [
-							{ translateX: x - CURSOR_ADJUST_OFFSET },
-							{ translateY: y * (-1) - CURSOR_ADJUST_OFFSET },
-						],
-					},
-				]}
-			>
-				<View className="absolute w-6 h-0.5 bg-purple-600 opacity-75" />
-				<View className="absolute h-6 w-0.5 bg-purple-600 opacity-75" />
-				<View className="absolute -top-4">
-					<View className="bg-purple-600 text-white text-xs px-1.5 py-0.5 rounded-full font-medium shadow-sm">
-						<Text>{id}</Text>
-					</View>
-				</View>
+		<View className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+			style={[
+				styles.cursor,
+				{
+					transform: [
+						{ translateX: x - CURSOR_ADJUST_OFFSET },
+						{ translateY: y * (-1) - CURSOR_ADJUST_OFFSET },
+					],
+				},
+			]}
+		>
+			<View className="absolute w-6 h-0.5 bg-purple-600 opacity-75" />
+			<View className="absolute h-6 w-0.5 bg-purple-600 opacity-75" />
+			<View className="absolute -top-5 bg-purple-600 rounded-xl w-5 h-5 items-center justify-center">
+				<Text className="text-white text-xsfont-medium shadow-sm">{id}</Text>
 			</View>
 		</View>
 	)
